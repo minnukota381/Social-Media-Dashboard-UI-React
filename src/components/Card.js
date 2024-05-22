b18cd1dc89d844237aa8680780c67678f147c32b
minnukota381 <minnukota381@gmail.com>
2024-05-22 14:11:45 +0530

    update | SMD

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Card = ({ title, value, icon, bgColor }) => (
   <div className="card" style={{ backgroundColor: bgColor }}>
     <div className="card-inner">
-      <h2>{title}</h2>
-      <span className="material-icons-outlined">{icon}</span>
+      <h3>{title}</h3>
+      <i className={`bi ${icon}`} style={{ fontSize: '50px' }}></i>
     </div>
     <h1>{value}</h1>
   </div>
 );
+
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+  bgColor: PropTypes.string.isRequired,
+};
 
 export default Card;
--- src/components/Main.js
+++ src/components/Main.js
@@ -9,10 +9,10 @@ const Main = () => (
       <h2>DASHBOARD</h2>
     </div>
     <div className="main-cards">
-      <Card title="LIKES" value="4,021" icon="thumb_up" bgColor="rgb(213, 0, 0)" />
-      <Card title="SUBSCRIBERS" value="8,731" icon="subscriptions" bgColor="rgb(46, 125, 50)" />
-      <Card title="FOLLOWERS" value="3,841" icon="groups" bgColor="rgb(255, 111, 0)" />
-      <Card title="MESSAGES" value="1,962" icon="forum" bgColor="rgb(29, 38, 154)" />
+      <Card title="TWITTER FOLLOWERS" value="4,021" icon="bi bi-twitter" bgColor="rgb(29, 161, 242)" />
+      <Card title="YOUTUBE SUBSCRIBERS" value="8,731" icon="bi bi-youtube" bgColor="rgb(255, 0, 0)" />
+      <Card title="INSTAGRAM FOLLOWERS" value="3,841" icon="bi bi-instagram" bgColor="rgb(225, 48, 108)" />
+      <Card title="LINKEDIN CONNECTIONS" value="1,962" icon="bi bi-linkedin" bgColor="rgb(0, 119, 181)" />
     </div>
     <div className="products">
       <ProductCard />
@@ -23,3 +23,4 @@ const Main = () => (
 );
 
 export default Main;
+
